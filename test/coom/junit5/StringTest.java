3d3fb57cbc38e60209e2c741e257b9b927a0d650
AwadYMohammed <awadyasirawad@gmail.com>
2020-08-25 04:48:29 -0400

    the name of the test method doesn't matter, but it should be annotated with @Test

---
+++ test/coom/junit5/StringTest.java
@@ -0,0 +1,15 @@
+package coom.junit5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringTest {
+
+	@Test
+	void whateveryouwant() {
+		fail("Not yet implemented");
+		// the name of the test method doesn't matter, but it should be annotated with @Test 
+	}
+
+}
